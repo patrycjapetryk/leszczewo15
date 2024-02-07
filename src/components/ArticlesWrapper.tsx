bdd8020511abd5c8342d328fdc30f8ebe2123ecd
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-02-07 11:12:40 +0100

    Add hidden scroll

--- src/components/ArticlesWrapper.tsx
+++ src/components/ArticlesWrapper.tsx
@@ -2,7 +2,7 @@ export type childrenProps = { children: React.ReactNode };
 
 export default function ArticlesWrapper({ children }: childrenProps) {
   return (
-    <article className="w-full overflow-y-scroll px-[1px] pb-10 text-sm leading-5 xl:fixed xl:left-5 xl:top-[12vh] xl:h-[83vh] xl:w-[30%]">
+    <article className="scroll w-full overflow-y-scroll px-[1px] pb-10 text-sm leading-5 xl:fixed xl:left-5 xl:top-[12vh] xl:h-[83vh] xl:w-[30%]">
       {children}
     </article>
   );
